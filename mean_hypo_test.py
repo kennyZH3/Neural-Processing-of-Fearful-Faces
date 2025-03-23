7b4493e6ca278252f13faa8e363ff4e9606552d9
Kenny Zhang <kennyzhang@Kennys-MBP.lan>
2025-03-23 17:04:43 -0400

    Moved data processing steps to 01 file

--- mean_hypo_test.py
+++ mean_hypo_test.py
@@ -1,62 +1,8 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 from scipy.stats import ttest_ind
 
-df = pd.read_csv("data/cleaned_data.csv")
-print(df.head())
-
-anxiety_distribution = df['anxiety'].value_counts()
-# Calculate mean, median, and standard deviation
-mean_anxiety = df['anxiety'].mean()
-median_anxiety = df['anxiety'].median()
-std_anxiety = df['anxiety'].std()
-
-# Print the statistics
-print("Mean Anxiety Score:", mean_anxiety)
-print("Median Anxiety Score:", median_anxiety)
-print("Standard Deviation of Anxiety Score:", std_anxiety)
-
-# Visualize the distribution
-plt.hist(df['anxiety'], bins=10)
-plt.xlabel('Anxiety Score')
-plt.ylabel('Frequency')
-plt.title('Distribution of Anxiety Scores')
-plt.savefig("plots/anxiety_distribution.png", dpi=300)
-plt.show()
-
-plt.figure()
-plt.boxplot(df['anxiety'], vert=False)
-plt.xlabel('Mean Anxiety Score')
-plt.title('Boxplot of Participant Mean Anxiety Scores')
-plt.savefig("plots/anxiety_boxplot.png", dpi=300)  
-plt.show()
-
-# Filter anxiety scores to create a binary variable with balanced classes
-median_val = df['anxiety'].median()
-df['anxiety_binary'] = df['anxiety'].apply(lambda x: 'high' if x >= median_val else 'low')
-class_counts = df['anxiety_binary'].value_counts()
-print("Class counts:\n", class_counts)
-num_variables = len(df.columns)
-num_observations = len(df)
-print(f"Number of variables: {num_variables}")
-print(f"Number of observations: {num_observations}")
-
-# Pivot to long format 
-df_long = df.melt(
-    id_vars=["id", "anxiety", "anxiety_binary"],
-    var_name="condition_roi",
-    value_name="activation"
-).assign(
-    condition=lambda x: x.condition_roi.str.split("__").str[0],
-    region=lambda x: x.condition_roi.str.split("__").str[1]
-).drop(columns="condition_roi")
-
+df_long = pd.read_csv("data/long_data.csv")
 print(df_long.head())
-
-num_variables = df_long.shape[1]
-num_observations = df_long.shape[0]
-print(f"Number of variables: {num_variables}")
-print(f"Number of observations: {num_observations}")
 
 
 # Hypothesis testing: Compare activation levels between high and low anxiety groups for each condition and region
